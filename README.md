dc7863a4e22b47ddf33cbb96d5ace2222ae33789
Kopselek <konrad.kkacperek@interia.pl>
2021-03-05 16:04:41 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# discord-bot
+
+Discord Bot is simple bot for your Discord server.
+
+## Libraries
+
+Program uses library - [discord.py](https://pypi.org/project/discord.py/)
+
+## Commands
+
+```
+.say <text>
+```
+Bot will say text instead of you
+
+```
+.kick <@mention> <text>
+```
+Bot will kick player in first argument with reason in second argument (you have to mention player by @)
+
+```
+.ban <@mention> <text>
+```
+Bot will ban player in first argument with reason in second argument (you have to mention player by @)
+
+```
+.toall <@rank> <text>
+```
+Bot will send message (second argument) to all players who contains rank on first argument
+
+```
+.clear <number>
+```
+Bot will purge x messages on channel
+
+## Information
+
+Program was tested on Python 3.9.
+
+All bugs can be reported in the "Issues" tab or on Discord of original author (Kopselek#6052).
---
+++ discord-bot.py
@@ -0,0 +1,100 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import has_permissions
+
+intents = discord.Intents.default()
+intents.members = True
+
+bot = commands.Bot(command_prefix='.', intents=intents)
+TOKEN = 'Your Discord Bot Token'
+server = 'server name'
+
+# Function to convert
+def list_to_string(s):
+    str1 = ""
+
+    for ele in s:
+        str1 += " " + ele
+
+    return str1
+
+
+@bot.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(bot))
+
+
+@bot.command(name="kick", pass_context=True)
+@has_permissions(manage_roles=True, ban_members=True)
+async def _kick(ctx, member: discord.Member, *text):
+    reason = list_to_string(text)
+
+    embed_var = discord.Embed(title=f"{member.display_name} Zostal wyrzucony", description=f"{reason}", color=0xffff00)
+    embed_var_for_member = discord.Embed(title=f"Zostales wyrzucony z {server}", description=f"{reason}",
+                                         color=0xffff00)
+
+    await ctx.message.delete()
+
+    # try send message to member
+    try:
+        await member.send(embed=embed_var_for_member)
+    except:
+        print(f'nie udalo sie wyslac wiadomosci do {member}')
+
+    await ctx.channel.send(embed=embed_var)
+    await member.kick(reason=reason)
+
+
+@bot.command(name="ban", pass_context=True)
+@has_permissions(manage_roles=True, ban_members=True)
+async def _ban(ctx, member: discord.Member, *text):
+    reason = list_to_string(text)
+
+    embed_var = discord.Embed(title=f"{member.display_name} Zostal zbanowany", description=f"{reason}", color=0xff0000)
+    embed_var_for_member = discord.Embed(title=f"Zostales zbanowany na {server}", description=f"{reason}",
+                                         color=0xff0000)
+
+    await ctx.message.delete()
+
+    # try send message to member
+    try:
+        await member.send(embed=embed_var_for_member)
+    except:
+        print(f'nie udalo sie wyslac wiadomosci do {member}')
+
+    await ctx.channel.send(embed=embed_var)
+    await member.ban(reason=reason)
+
+
+@bot.command(name="say", pass_context=True)
+@has_permissions(manage_roles=True, ban_members=True)
+async def _say(ctx, *text):
+    args = list_to_string(text)
+
+    embed_var = discord.Embed(description=f"{args}", color=0x000000)
+
+    await ctx.message.delete()
+    await ctx.channel.send(embed=embed_var)
+
+
+@bot.command(name="clear", pass_context=True)
+@has_permissions(manage_roles=True, ban_members=True)
+async def _clear(ctx, number = 5):
+
+    await ctx.message.delete()
+
+    await ctx.channel.purge(limit=number)
+
+@bot.command(name="toall", pass_context=True)
+@has_permissions(manage_roles=True, ban_members=True)
+async def _toall(ctx, role: discord.Role, *text):
+    async for member in ctx.message.guild.fetch_members(limit=300):
+        if member.bot is False:
+            if role in member.roles:
+                message = list_to_string(text)
+
+                embed_var = discord.Embed(description=f"{message}", color=0xff0000)
+
+                await member.send(embed=embed_var)
+
+bot.run(TOKEN)
